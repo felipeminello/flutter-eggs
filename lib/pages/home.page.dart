1b17a20fa5e8711c8e08cd7f4b2160107c80fca7
Felipe Minello <felipeminello@gmail.com>
2021-01-07 13:35:03 -0300

    barra de progresso

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,9 +1,15 @@
+import 'package:eggs/blocs/app.bloc.dart';
+import 'package:eggs/themes/action-buttons.widget.dart';
+import 'package:eggs/widgets/progress.widget.dart';
 import 'package:eggs/widgets/type-buttons.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of<AppBloc>(context);
+
     return Scaffold(
       body: Container(
         child: Column(
@@ -11,7 +17,19 @@ class HomePage extends StatelessWidget {
             SizedBox(
               height: 60,
             ),
-            TypeButtons()
+            Text(bloc.selected),
+            Text(bloc.state),
+            Text(bloc.time.toString()),
+            Text(bloc.seconds.toString()),
+            SizedBox(
+              height: 60,
+            ),
+            TypeButtons(),
+            SizedBox(
+              height: 60,
+            ),
+            ActionButtons(),
+            Progress(),
           ],
         ),
       ),
